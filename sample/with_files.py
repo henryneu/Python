57076c2766c95cd1f556aabfe18b51c17aef3141
henryneu <neuyimi@gmail.com>
2017-03-19 10:45:14 +0800

    commit

---
+++ sample/with_files.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+with open('test.txt', 'w') as f:
+    f.write('today is ')
+    f.write(datetime.now().strftime('%Y-%M-%D'))
+
+with open('test.txt', 'r') as f:
+    s = f.read()
+    print('open for read...')
+    print(s)
+
+with open('test.txt', 'rb') as f:
+    s = f.read()
+    print('open as binary for read...')
+    print(s)
+
+with open('/workspace/python/case.txt', 'r') as f1:
+    s = f1.readline()  #  每次读取一行文件
+    print('open as readline...')
+    print(s)
+
+with open('/workspace/python/case.txt', 'r') as f1:
+    print('open as readlines...')
+    for line in f1.readlines():  #  一次读取所有内容并按行返回list
+        print(line.strip())
